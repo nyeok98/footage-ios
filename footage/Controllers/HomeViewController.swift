15e0e0a2764bd5636f726e59ac7a9b1fd5306e0f
Wootae <Wootae@Paul-Macui-MacBook-Pro.local>
2020-06-15 14:20:05 +0900

    [update] add polyline in mapkit

--- footage/Controllers/HomeViewController.swift
+++ footage/Controllers/HomeViewController.swift
@@ -12,7 +12,7 @@ import EFCountingLabel
 
 class HomeViewController: UIViewController {
     
-    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mainMap: MKMapView!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var youString: UILabel!
     @IBOutlet weak var todayString: UILabel!
@@ -30,7 +30,38 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var distanceView: UIView!
     @IBOutlet weak var unitLabel: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mainMap.delegate = self
+        startButton.setImage(#imageLiteral(resourceName: "start_btn"), for: .normal)
+        setInitialAlpha()
+        setInitialPositionTriangle()
+        HomeAnimation.homeStopAnimation(self)
+        drawDirection()
+    }
     
+    func setInitialAlpha() {
+        startButton.alpha = 0
+        square1.alpha = 0
+        square2.alpha = 0
+        square3.alpha = 0
+        square4.alpha = 0
+        triangle1.alpha = 0
+        triangle2.alpha = 0
+        triangle3.alpha = 0
+        triangle4.alpha = 0
+        triangle5.alpha = 0
+        distanceView.alpha = 0
+        unitLabel.alpha = 0
+    }
+    
+    func setInitialPositionTriangle() {
+        triangle1.frame.origin.y = -40
+        triangle2.frame.origin.y = -40
+        triangle3.frame.origin.y = -40
+        triangle4.frame.origin.y = -40
+        triangle5.frame.origin.y = -40
+    }
     
     @IBAction func startButtonPressed(_ sender: UIButton) {
         
@@ -43,38 +74,12 @@ class HomeViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        startButton.setImage(#imageLiteral(resourceName: "start_btn"), for: .normal)
-        startButton.alpha = 0
-        square1.alpha = 0
-        square2.alpha = 0
-        square3.alpha = 0
-        square4.alpha = 0
-        triangle1.frame.origin.y = -40
-        triangle2.frame.origin.y = -40
-        triangle3.frame.origin.y = -40
-        triangle4.frame.origin.y = -40
-        triangle5.frame.origin.y = -40
-        triangle1.alpha = 0
-        triangle2.alpha = 0
-        triangle3.alpha = 0
-        triangle4.alpha = 0
-        triangle5.alpha = 0
-        distanceView.alpha = 0
-        unitLabel.alpha = 0
-        HomeAnimation.homeStopAnimation(self)
-        
-    }
-    
 }
 
 // MARK: Map Kit View
 
-extension HomeViewController: CLLocationManagerDelegate {
-
+extension HomeViewController: CLLocationManagerDelegate, MKMapViewDelegate {
+    
     func configureMapView() {
         let locationManager = CLLocationManager()//지도 띄우기
         // Do any additional setup after loading the view, typically from a nib.
@@ -82,25 +87,57 @@ extension HomeViewController: CLLocationManagerDelegate {
         locationManager.desiredAccuracy = kCLLocationAccuracyBest //정확도 최고
         locationManager.requestWhenInUseAuthorization() //사용자 인증 요청
         locationManager.startUpdatingLocation() //위치 업데이트 시작
-        mapView.showsUserLocation = true //현재 위치에 마커로 표시됨
+        mainMap.showsUserLocation = true //현재 위치에 마커로 표시됨
         
     }
-
+    
     
     func myLocation(latitude: CLLocationDegrees, longitude: CLLocationDegrees, delta: Double){
         let coordinateLocation = CLLocationCoordinate2DMake(latitude, longitude)
         let spanValue = MKCoordinateSpan(latitudeDelta: delta, longitudeDelta: delta)
         let locationRegion = MKCoordinateRegion(center: coordinateLocation, span: spanValue)
-        mapView.setRegion(locationRegion, animated: true)
+        mainMap.setRegion(locationRegion, animated: true)
         
     }
     
     //업데이트 되는 위치정보 표시
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let lastLocation = locations.last //가장 최근의 위치정보 저장
-      myLocation(latitude: (lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!, delta: 0.01) //delat값이 1보다 작을수록 확대됨. 0.01은 100배확대
+        myLocation(latitude: (lastLocation?.coordinate.latitude)!, longitude: (lastLocation?.coordinate.longitude)!, delta: 0.01) //delat값이 1보다 작을수록 확대됨. 0.01은 100배확대
         
     }
+    
+    func drawDirection() {
+        let start = MKPlacemark(coordinate:
+            CLLocationCoordinate2D(latitude: 36.5150, longitude: 127.2384))
+        let finish = MKPlacemark(coordinate:
+            CLLocationCoordinate2D(latitude: 36.5150, longitude: 128))
+        let direction = MKDirections.Request()
+        direction.source = MKMapItem(placemark: start)
+        direction.destination = MKMapItem(placemark: finish)
+        direction.transportType = .walking
+        let directions = MKDirections(request: direction)
+        directions.calculate { (response, error) in
+            guard let response = response else {
+                return
+            }
+            let route = response.routes[0]
+            print(route.steps[0].instructions)
+            self.mainMap.addOverlay(route.polyline)
+//            let originalFrame = route.polyline.boundingMapRect
+//            let mapFrame = MKMapRect(x: originalFrame.origin.x-100, y: originalFrame.origin.y-100, width: originalFrame.width + 100, height: originalFrame.height + 100)
+            self.mainMap.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let polylineView = MKPolylineRenderer(overlay: overlay)
+        polylineView.strokeColor = UIColor(named: "tabColor")
+        return polylineView
+    }
+    
+    
 }
 
 
