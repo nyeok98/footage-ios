76466478f534cefd50b098df715c487918067976
Dongnyeok Shin <dn.shin98@gmail.com>
2021-12-29 17:06:40 +0900

    1.2.1 release version // [update] prep, next button in monthly report is added

--- Footage/Scene/Stats/ReportVC.swift
+++ Footage/Scene/Stats/ReportVC.swift
@@ -25,10 +25,23 @@ class ReportVC: UIViewController {
     @IBOutlet weak var octoberButton: UIButton!
     @IBOutlet weak var novemberButton: UIButton!
     @IBOutlet weak var decemberButton: UIButton!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var monthLabel: UILabel!
     
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         performSegue(withIdentifier: "goToReportDetail", sender: sender)
+    }
+    @IBAction func nextMonthButtonPressed(_ sender: Any) {
+        thisYear += 1
+        monthLabel.text = String(thisYear)
+        setButtons()
+    }
+    @IBAction func prevMonthButtonPressed(_ sender: Any) {
+        thisYear -= 1
+        monthLabel.text = String(thisYear)
+        setButtons()
     }
     
     
@@ -36,38 +49,73 @@ class ReportVC: UIViewController {
     var placeRank: Array<(key: String, value: Double)> = []
     var startDate: Int?
     var endDate: Int?
+    var thisYear = 2021
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        thisYear = Calendar.current.component(.year, from: Date())
         setButtons()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let destinationVC = segue.destination as! ReportDetailVC
         guard let selectedButton = sender as? UIButton else { return }
-        setStartEndDate(tag: selectedButton.tag)
+        setStartEndDate(year: thisYear, tag: selectedButton.tag)
         destinationVC.whichMonth = String(selectedButton.tag)
         destinationVC.colorRanking = ColorManager.getRankingDistance(startDate: startDate!, endDate: endDate!)
         destinationVC.placeRanking = PlaceManager.getRankingDistance(startDate: startDate!, endDate: endDate!)
     }
     
     func setButtons() {
+        monthLabel.text = String(thisYear)
         let monthButtonList = [januaryButton, februaryButton, marchButton, aprilButton, mayButton, juneButton, julyButton, augustButton, septemberButton, octoberButton, novemberButton, decemberButton]
         for index in 0...11 {
-            setStartEndDate(tag: (index+1))
+            setStartEndDate(year: thisYear, tag: (index+1))
             if ColorManager.getRankingDistance(startDate: startDate!, endDate: endDate!).isEmpty {
                 monthButtonList[index]!.isEnabled = false
                 monthButtonList[index]!.alpha = 0.1
             } else {
                 monthButtonList[index]!.isEnabled = true
                 monthButtonList[index]!.alpha = 1
+                
             }
-            
         }
+        setPrevNextButtons()
     }
     
-    func setStartEndDate(tag: Int) {
-        startDate = DateConverter.tagToMonth(year: 2020, tag: tag, start: true)
-        endDate = DateConverter.tagToMonth(year: 2020, tag: tag, start: false)
+    func setStartEndDate(year: Int, tag: Int) {
+        startDate = DateConverter.tagToMonth(year: year, tag: tag, start: true)
+        endDate = DateConverter.tagToMonth(year: year, tag: tag, start: false)
+    }
+    
+    func setPrevNextButtons(){
+        var prevCnt = 0
+        var nextCnt = 0
+        for index in 0...11 {
+            setStartEndDate(year: thisYear+1, tag: (index+1))
+            if !ColorManager.getRankingDistance(startDate: startDate!, endDate: endDate!).isEmpty {
+                nextCnt+=1
+            }
+        }
+        for index in 0...11 {
+            setStartEndDate(year: thisYear-1, tag: (index+1))
+            if !ColorManager.getRankingDistance(startDate: startDate!, endDate: endDate!).isEmpty {
+                prevCnt+=1
+            }
+        }
+        if prevCnt>0 {
+            prevButton.isEnabled = true
+            prevButton.alpha = 1
+        } else {
+            prevButton.isEnabled = false
+            prevButton.alpha = 0.1
+        }
+        if nextCnt>0 {
+            nextButton.isEnabled = true
+            nextButton.alpha = 1
+        } else {
+            nextButton.isEnabled = false
+            nextButton.alpha = 0.1
+        }
     }
 }
